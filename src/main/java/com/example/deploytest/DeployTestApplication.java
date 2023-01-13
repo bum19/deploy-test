f2b383b112a81f92c09874e29105affb338733cf
bum19 <1996joon@gmail.com>
2023-01-13 15:26:07 +0900

    dev branch

--- src/main/java/com/example/deploytest/DeployTestApplication.java
+++ src/main/java/com/example/deploytest/DeployTestApplication.java
@@ -3,10 +3,13 @@ package com.example.deploytest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class DeployTestApplication {
 
 	public static void main(String[] args) {
+		System.out.println("HI Im dev branch");
 		SpringApplication.run(DeployTestApplication.class, args);
 	}
 
